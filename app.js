21c359f4cf67774d55c39406bb60d0f3a660d5cd
muhammad_moosa_hashim <moosa.hashim@techlogix.com>
2024-04-15 15:28:33 +0500

    basic fs commands

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
-const log = require('./logger');
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
 
-log('message');  
+var pathObj = path.parse(__filename);
+
+console.log(pathObj);
+
+
+var totalMemory = os.totalmem();
+var freeMemory = os.freemem();
+
+console.log(`Total Memory: ${totalMemory}`);
+console.log(`Free Memory: ${freeMemory}`);
+
+
+const files = fs.readdirSync('./');
+console.log(files);
+
+fs.readdir('./', function(err, files){
+    if(err) console.log('Error', err);
+    else console.log('Result', files);
+});
