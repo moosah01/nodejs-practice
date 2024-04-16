12acf9e94f585e1b0cd9adc740f88bcddf021d35
muhammad_moosa_hashim <moosa.hashim@techlogix.com>
2024-04-16 18:41:20 +0500

    nested params and query string

--- express-demo/index.js
+++ express-demo/index.js
@@ -10,6 +10,15 @@ app.get('/api/courses', (req, res)=> {
     res.send([1, 2, 3]);
 });
 
+app.get('/api/posts/:year/:month', (req,res)=>{
+    res.send(req.params);
+});
+
+// /api/posts/2018/1?sortBy=name => query string is stored in key value pairs
+app.get('/api/posts/:year/:month', (req,res)=>{
+    res.send(req.query);
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, ()=>{
