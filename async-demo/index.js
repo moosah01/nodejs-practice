e600e8e8b99f18ed73943a071d069179502d7d38
muhammad_moosa_hashim <moosa.hashim@techlogix.com>
2024-04-17 20:27:24 +0500

    callbakc hell demo

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,25 +1,30 @@
-console.log('Before')
-const user = getUser(1, (user) => {
-    console.log('User', user) 
-
-    getRepositories(user.gitHubUsername, (repos) => {
-        console.log('Repos', repos)
-    })
-})
-
-console.log('After')
-
+console.log("Before");
+getUser(1, (user) => {
+  getRepositories(user.gitHubUsername, (repos) => {
+    getCommits(repos[0], (commits) => {
+      console.log(commits);
+    });
+  });
+});
+console.log("After");
 
 function getUser(id, callback) {
-    setTimeout(()=> {
-        console.log('Reading a user from a database...')
-        callback({id: id, gitHubUsername: 'moosa'});
-    }, 2000);
+  setTimeout(() => {
+    console.log("Reading a user from a database...");
+    callback({ id: id, gitHubUsername: "moosa" });
+  }, 2000);
 }
 
 function getRepositories(username, callback) {
-    setTimeout(()=> {
-        console.log('Calling GitHub API...')
-        callback(['repo1', 'repo2', 'repo3']);
-    }, 2000);
+  setTimeout(() => {
+    console.log("Calling GitHub API...");
+    callback(["repo1", "repo2", "repo3"]);
+  }, 2000);
+}
+
+function getCommits(repo, callback) {
+  setTimeout(() => {
+    console.log("Calling GitHub API...");
+    callback(["commit"]);
+  }, 2000);
 }
